ba62bb3b8722b1eec39c59272505107dd78c2277
Caroline Chuang <yachi.chuang@mail.utoronto.ca>
2018-03-19 01:40:03 -0400

    separate load data, build another net

--- version1/char_recog_emnist.py
+++ version1/char_recog_emnist.py
@@ -5,90 +5,16 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from keras.layers import Conv2D, MaxPooling2D, Convolution2D, Dropout, Dense, Flatten, LSTM
 from keras.models import Sequential, save_model
 from keras.utils import np_utils
-from scipy.io import loadmat
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
+from keras.layers import Input, Flatten, Dense
 import pickle
 import argparse
 import keras
 import numpy as np
+import load_mnist_data
+import plot_graph
 
-def load_data(mat_file_path, width=28, height=28, max_=None, verbose=True):
-    ''' Load data in from .mat file as specified by the paper.
-        Arguments:
-            mat_file_path: path to the .mat, should be in sample/
-        Optional Arguments:
-            width: specified width
-            height: specified height
-            max_: the max number of samples to load
-            verbose: enable verbose printing
-        Returns:
-            A tuple of training and test data, and the mapping for class code to ascii value,
-            in the following format:
-                - ((training_images, training_labels), (testing_images, testing_labels), mapping)
-    '''
-    # Local functions
-    def rotate(img):
-        # Used to rotate images (for some reason they are transposed on read-in)
-        flipped = np.fliplr(img)
-        return np.rot90(flipped)
-
-    def display(img, threshold=0.5):
-        # Debugging only
-        render = ''
-        for row in img:
-            for col in row:
-                if col > threshold:
-                    render += '@'
-                else:
-                    render += '.'
-            render += '\n'
-        return render
-
-    # Load convoluted list structure form loadmat
-    mat = loadmat(mat_file_path)
-
-    # Load char mapping
-    mapping = {kv[0]:kv[1:][0] for kv in mat['dataset'][0][0][2]}
-    pickle.dump(mapping, open('bin/mapping.p', 'wb'))
-
-    # Load training data
-    if max_ == None:
-        max_ = len(mat['dataset'][0][0][0][0][0][0])
-    training_images = mat['dataset'][0][0][0][0][0][0][:max_].reshape(max_, height, width, 1)
-    training_labels = mat['dataset'][0][0][0][0][0][1][:max_]
-
-    # Load testing data
-    if max_ == None:
-        max_ = len(mat['dataset'][0][0][1][0][0][0])
-    else:
-        max_ = int(max_ / 6)
-    testing_images = mat['dataset'][0][0][1][0][0][0][:max_].reshape(max_, height, width, 1)
-    testing_labels = mat['dataset'][0][0][1][0][0][1][:max_]
-
-    # Reshape training data to be valid
-    if verbose == True: _len = len(training_images)
-    for i in range(len(training_images)):
-        if verbose == True: print('%d/%d (%.2lf%%)' % (i + 1, _len, ((i + 1)/_len) * 100), '\r')
-        training_images[i] = rotate(training_images[i])
-    if verbose == True: print('')
-
-    # Reshape testing data to be valid
-    if verbose == True: _len = len(testing_images)
-    for i in range(len(testing_images)):
-        if verbose == True: print('%d/%d (%.2lf%%)' % (i + 1, _len, ((i + 1)/_len) * 100), '\r')
-        testing_images[i] = rotate(testing_images[i])
-    if verbose == True: print('')
-
-    # Convert type to float32
-    training_images = training_images.astype('float32')
-    testing_images = testing_images.astype('float32')
-
-    # Normalize to prevent issues with model
-    training_images /= 255
-    testing_images /= 255
-
-    nb_classes = len(mapping)
-
-    return ((training_images, training_labels), (testing_images, testing_labels), mapping, nb_classes)
 
 def build_net(training_data, width=28, height=28, verbose=False):
     ''' Build and train neural network. Also offloads the net in .yaml and the
@@ -121,11 +47,11 @@ def build_net(training_data, width=28, height=28, verbose=False):
                             activation='relu'))
 
     model.add(MaxPooling2D(pool_size=pool_size))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
     model.add(Flatten())
 
     model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
     model.add(Dense(nb_classes, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy',
@@ -136,23 +62,104 @@ def build_net(training_data, width=28, height=28, verbose=False):
     return model
 
 
-def train(model, training_data, callback=True, batch_size=256, epochs=10):
+def build_vgg16_model1(training_data, width=28, height=28, verbose=False):
+    (x_train, y_train), (x_test, y_test), mapping, nb_classes = training_data
+    input_shape = (height, width, 1)
+
+    # Hyperparameters
+    nb_filters = 32 # number of convolutional filters to use
+    pool_size = (2, 2) # size of pooling area for max pooling
+    kernel_size = (3, 3) # convolution kernel size
+
+    model = Sequential()
+    model.add(Conv2D(filters=32,
+                            kernel_size=kernel_size,
+                            padding='valid',
+                            input_shape=input_shape,
+                            activation='relu'))
+    model.add(Conv2D(filters=32,
+                            kernel_size=kernel_size,
+                            activation='relu'))
+
+    model.add(MaxPooling2D(pool_size=pool_size))
+    model.add(Dropout(0.5))
+    model.add(Conv2D(filters=64,
+                            kernel_size=kernel_size,
+                            padding='valid',
+                            activation='relu'))
+    model.add(Conv2D(filters=64,
+                            kernel_size=kernel_size,
+                            activation='relu'))
+    model.add(MaxPooling2D(pool_size=pool_size))
+    # model.add(Dropout(0.5))
+    # model.add(Conv2D(filters=32,
+    #                         kernel_size=kernel_size,
+    #                         padding='valid',
+    #                         activation='relu'))
+    # model.add(Conv2D(filters=32,
+    #                         kernel_size=kernel_size,
+    #                         activation='relu'))
+    # model.add(MaxPooling2D(pool_size=pool_size))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nb_classes, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adadelta',
+                  metrics=['accuracy'])
+
+    if verbose == True: print(model.summary())
+    return model
+
+
+
+
+def build_vgg16_model2(training_data, width=28, height=28):
+    (x_train, y_train), (x_test, y_test), mapping, nb_classes = training_data
+
+    # Get back the convolutional part of a VGG network trained on ImageNet
+    model_vgg16_conv = VGG16(weights='imagenet', include_top=False, pooling='max')
+    model_vgg16_conv.summary()
+
+    # Use the generated model
+    input_shape = Input(shape=(244, 244, 3), name='image_input')
+
+    output_vgg16_conv = model_vgg16_conv(input_shape)
+
+    # Add the fully-connected layers
+    x = Flatten(name='flatten')(output_vgg16_conv)
+    x = Dense(4096, activation='relu', name='fc1')(x)
+    x = Dense(4096, activation='relu', name='fc2')(x)
+    x = Dense(nb_classes, activation='softmax', name='predictions')(x)
+
+    # Create your own model
+    model = Model(input=input_shape, output=x)
+
+    # In the summary, weights and layers from VGG part will be hidden, but they will be fit during the training
+    model.summary()
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adadelta',
+                  metrics=['accuracy'])
+    return model
+
+def train(model, training_data, dir_name, batch_size=256, epochs=30):
     (x_train, y_train), (x_test, y_test), mapping, nb_classes = training_data
 
     # convert class vectors to binary class matrices
     y_train = np_utils.to_categorical(y_train, nb_classes)
     y_test = np_utils.to_categorical(y_test, nb_classes)
 
-    if callback == True:
-        # Callback for analysis in TensorBoard
-        tbCallBack = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
+    history_callback = model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test),
+          callbacks=None)
 
-    model.fit(x_train, y_train,
-              batch_size=batch_size,
-              epochs=epochs,
-              verbose=1,
-              validation_data=(x_test, y_test),
-              callbacks=[tbCallBack] if callback else None)
+    analyze_training(history_callback, dir_name)
 
     score = model.evaluate(x_test, y_test, verbose=0)
     print('Test score:', score[0])
@@ -160,10 +167,27 @@ def train(model, training_data, callback=True, batch_size=256, epochs=10):
 
     # Offload model to file
     model_yaml = model.to_yaml()
-    with open("bin/model.yaml", "w") as yaml_file:
+    with open('bin/' + dir_name + '/model.yaml', "w") as yaml_file:
         yaml_file.write(model_yaml)
-    save_model(model, 'bin/model.h5')
+    save_model(model, 'bin/' + dir_name + '/model.h5')
 
+
+def analyze_training(history_callback, dir_name):
+    # Callback for analysis in TensorBoard
+    output_dir = './Graph/' + dir_name
+    keras.callbacks.TensorBoard(log_dir=output_dir, histogram_freq=0, write_graph=True, write_images=True)
+
+    training_loss = np.array(history_callback.history['loss'])
+    training_acc = np.array(history_callback.history['acc'])
+
+    validation_loss = np.array(history_callback.history['val_loss'])
+    validation_acc = np.array(history_callback.history['val_acc'])
+
+    if len(validation_loss) > 0:
+        validation_loss = []
+        validation_acc = []
+    plot_graph.generate_graph(training_loss, training_acc, validation_loss, validation_acc,
+                              title=dir_name, output_dir=output_dir)
 
 def baseline_model(num_pixels):
     (x_train, y_train), (x_test, y_test), mapping, nb_classes = training_data
@@ -178,7 +202,7 @@ def baseline_model(num_pixels):
     y_train = np_utils.to_categorical(y_train, nb_classes)
     y_test = np_utils.to_categorical(y_test, nb_classes)
 
-    model.fit(x_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
+    model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10, batch_size=200, verbose=2)
 
     return model
 
@@ -186,6 +210,7 @@ def baseline_model(num_pixels):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(usage='A training program for classifying the EMNIST dataset')
     parser.add_argument('-f', '--file', type=str, help='Path .mat file data', required=True)
+    parser.add_argument('-outdir', type=str, help='Output directory name', required=True)
     parser.add_argument('--width', type=int, default=28, help='Width of the images')
     parser.add_argument('--height', type=int, default=28, help='Height of the images')
     parser.add_argument('--max', type=int, default=None, help='Max amount of data to use')
@@ -193,11 +218,16 @@ if __name__ == '__main__':
     parser.add_argument('--verbose', action='store_true', default=False, help='Enables verbose printing')
     args = parser.parse_args()
 
-    bin_dir = os.path.dirname(os.path.realpath(__file__)) + '/bin'
+    bin_dir = os.path.dirname(os.path.realpath(__file__)) + '/bin/'
     if not os.path.exists(bin_dir):
         os.makedirs(bin_dir)
 
-    training_data = load_data(args.file, width=args.width, height=args.height, max_=args.max, verbose=args.verbose)
+    if not os.path.exists(bin_dir + '/' + args.outdir):
+        os.makedirs(bin_dir + '/' + args.outdir)
 
-    model = build_net(training_data, width=args.width, height=args.height, verbose=args.verbose)
-    train(model, training_data, epochs=args.epochs)
+    training_data = load_mnist_data.load_data(args.file, args.outdir, width=args.width, height=args.height,
+                                              max_=args.max, verbose=args.verbose)
+
+    model = build_vgg16_model1(training_data=training_data, width=28, height=28)
+    # model = build_net(training_data, width=args.width, height=args.height, verbose=args.verbose)
+    train(model, training_data, dir_name=args.outdir, epochs=args.epochs)
